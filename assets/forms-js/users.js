88ca6a76568dfff68fd486a08331ed1a9b2ee8e5
Shafkimsr <shafki82mhmd@gmail.com>
2024-11-05 10:40:02 +0530

    initial

--- assets/forms-js/users.js
+++ assets/forms-js/users.js
@@ -1,12 +1,55 @@
 
 $(document).ready(function () {
+    // Trigger change event on page load if doctor permission is selected by default
+    if ($('#permission, #edit_permission').val() === 'doctor') {
+        $('#permission, #edit_permission').trigger('change');
+    }
+
+
+    $('#permission').change(function () {
+        var permission = $(this).val();
+        alert(permission);
+        if (permission === 'doctor') {
+            $('#additional-fields').html(
+                '<div class="row mt-2">' +
+                '<div class="col-12 mb-3">' +
+                '<label for="name" class="form-label">Doctor Name</label>' +
+                '<input type="text" id="name" name="doctor_name" class="form-control" placeholder="Enter Name" required />' +
+                '</div>' +
+                '<div class="col-12 mb-3">' +
+                '<label for="about" class="form-label">About Doctor</label>' +
+                '<textarea id="about" name="about_doctor" class="form-control" placeholder="Enter About" required></textarea>' +
+                '</div>' +
+                '<div class="col-12 mb-3">' +
+                '<label for="formFile" class="form-label">Doctor Photo</label>' +
+                '<input class="form-control" name="image" id="image" type="file" accept="image/*">' +
+                '</div>' +
+                '</div>'
+            );
+        } else {
+            $('#additional-fields').empty();
+        }
+    });
+
+    function validatePasswords(class1, class2) {
+        if ($('#' + class1).val() === $('#' + class2).val()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     $('#create').on('click', function () {
         var form = $('#create-form')[0] ?? null;
         if (!form) console.log('Something went wrong..');
 
+        if (!validatePasswords('passwordInput', 'confirmPasswordInput')) {
+            showAlert('Passwords do not match..!', 'danger'); // Prevent form submission if passwords do not match
+            return;
+        }
+
         var url = $('#create-form').attr('action');
-        form.reportValidity()
-        if (form.checkValidity()) {
+        if (form.checkValidity() && form.reportValidity()) {
             var formData = new FormData(form);
             // Perform AJAX request
             $.ajax({
@@ -42,31 +85,45 @@ $(document).ready(function () {
             showAlert('Form is not valid. Please check your inputs.', 'danger');
         }
     });
-});
 
-$(document).ready(function () {
-    $('#create-new').on('click', function () {
+    $('.edit-user-btn').on('click', async function () {
+        var user_id = $(this).data('id');
+        await getUserById(user_id);
+    })
+
+    $('.delete-user-btn').on('click', async function () {
+        var user_id = $(this).data('id');
+        var is_confirm = confirm('Are you sure,Do you want to delete?');
+        if (is_confirm) await deleteById(user_id);
+    })
+
+    $('#update-user').on('click', function () {
+        if (!validatePasswords('password', 'confirm-password')) {
+            showAlert('Passwords do not match..!', 'danger', 'edit-alert-container'); // Prevent form submission if passwords do not match
+            return;
+        }
         // Get the form element
-        var form = $('#create-user-form')[0];
+        var form = $('#update-form')[0];
         form.reportValidity();
 
         // Check form validity
         if (form.checkValidity()) {
-            // Create a FormData object
-            var formData = new FormData($('#create-user-form')[0]);
+            // Serialize the form data
+            var url = $('#update-form').attr('action');
+            var formData = new FormData($('#update-form')[0]);
 
             // Perform AJAX request
             $.ajax({
-                url: $('#create-user-form').attr('action'),
+                url: url,
                 type: 'POST',
-                data: formData,
-                contentType: false, // Don't set content type
-                processData: false, // Don't process the data
+                data: formData, // Form data
                 dataType: 'json',
+                contentType: false,
+                processData: false,
                 success: function (response) {
-                    showAlert(response.message, response.success ? 'primary' : 'danger');
+                    showAlert(response.message, response.success ? 'primary' : 'danger', 'edit-alert-container');
                     if (response.success) {
-                        $('#createUserModal').modal('hide');
+                        $('#edit-user-modal').modal('hide');
                         setTimeout(function () {
                             location.reload();
                         }, 1000);
@@ -87,11 +144,86 @@ $(document).ready(function () {
         }
     });
 
+});
 
-    $('.edit-user').on('click', async function () {
-        var user_id = $(this).data('id');
-        await getUserById(user_id);
-    })
+async function getUserById(id) {
+    var url = $('#update-form').attr('action');
+
+    // Perform AJAX request
+    $.ajax({
+        url: url,
+        type: 'GET',
+        data: {
+            user_id: id,
+            action: 'get_user'
+        }, // Form data
+        dataType: 'json',
+        success: function (response) {
+            console.log(response);
+
+            showAlert(response.message, response.success ? 'primary' : 'danger');
+            if (response.success) {
+                var user_id = response.data.id;
+                var username = response.data.username;
+                var email = response.data.email;
+                var permission = response.data.permission;
+                var is_active = response.data.is_active;
+
+                $('#edit-user-modal #user_id').val(user_id);
+                $('#edit-user-modal #user-name').val(username);
+                $('#edit-user-modal #email').val(email);
+                $('#edit-user-modal #permission option[value="' + permission + '"]').prop('selected', true);
+                $('#edit-user-modal #is_active option[value="' + is_active + '"]').prop('selected', true);
+
+                $('#edit-user-modal').modal('show');
+            }
+        },
+        error: function (error) {
+            // Handle the error
+            console.error('Error submitting the form:', error);
+        },
+        complete: function (response) {
+            // This will be executed regardless of success or error
+            console.log('Request complete:', response);
+        }
+    });
+}
+
+
+async function deleteById(id) {
+    var url = $('#update-form').attr('action');
+
+    // Perform AJAX request
+    $.ajax({
+        url: url,
+        type: 'GET',
+        data: {
+            user_id: id,
+            action: 'delete_user'
+        }, // Form data
+        dataType: 'json',
+        success: function (response) {
+            if (response.success) {
+                setTimeout(function () {
+                    location.reload();
+                }, 1000);
+            }
+        },
+        error: function (error) {
+            // Handle the error
+            console.error('Error submitting the form:', error);
+        },
+        complete: function (response) {
+            // This will be executed regardless of success or error
+            console.log('Request complete:', response);
+        }
+    });
+}
+
+
+
+
+$(document).ready(function () {
 
     $('.delete-user').on('click', async function () {
         var user_id = $(this).data('id');
@@ -99,49 +231,6 @@ $(document).ready(function () {
         if (is_confirm) await deleteById(user_id);
     })
 
-    $('#update-now').on('click', function () {
-
-        // Get the form element
-        var form = $('#update-user-form')[0];
-        $('#update-user-form')[0].reportValidity();
-
-        // Check form validity
-        if (form.checkValidity()) {
-            // Serialize the form data
-            var formAction = $('#update-user-form').attr('action');
-            var formData = new FormData($('#update-user-form')[0]);
-
-            // Perform AJAX request
-            $.ajax({
-                url: formAction,
-                type: 'POST',
-                data: formData, // Form data
-                dataType: 'json',
-                contentType: false,
-                processData: false,
-                success: function (response) {
-                    showAlert(response.message, response.success ? 'primary' : 'danger', 'alert-container-update-form');
-                    if (response.success) {
-                        $('#editUserModal').modal('hide');
-                        setTimeout(function () {
-                            location.reload();
-                        }, 1000);
-                    }
-                },
-                error: function (error) {
-                    // Handle the error
-                    console.error('Error submitting the form:', error);
-                },
-                complete: function (response) {
-                    // This will be executed regardless of success or error
-                    console.log('Request complete:', response);
-                }
-            });
-        } else {
-            var message = ('Form is not valid. Please check your inputs.');
-            showAlert(message, 'danger');
-        }
-    });
 
     $('#permission').change(function () {
         var permission = $(this).val();
@@ -197,72 +286,3 @@ $(document).ready(function () {
     });
 });
 
-async function getUserById(id) {
-    var formAction = $('#update-user-form').attr('action');
-
-    // Perform AJAX request
-    $.ajax({
-        url: formAction,
-        type: 'GET',
-        data: {
-            user_id: id,
-            action: 'get_user'
-        }, // Form data
-        dataType: 'json',
-        success: function (response) {
-            showAlert(response.message, response.success ? 'primary' : 'danger');
-            if (response.success) {
-                var user_id = response.data.id;
-                var username = response.data.username;
-                var email = response.data.email;
-                var permission = response.data.permission;
-                var is_active = response.data.is_active;
-
-                $('#editUserModal #user_id').val(user_id);
-                $('#editUserModal #username').val(username);
-                $('#editUserModal #email').val(email);
-                $('#editUserModal #permission option[value="' + permission + '"]').prop('selected', true);
-                $('#editUserModal #is_active option[value="' + is_active + '"]').prop('selected', true);
-                $('#editUserModal').modal('show');
-            }
-        },
-        error: function (error) {
-            // Handle the error
-            console.error('Error submitting the form:', error);
-        },
-        complete: function (response) {
-            // This will be executed regardless of success or error
-            console.log('Request complete:', response);
-        }
-    });
-}
-
-async function deleteById(id) {
-    var formAction = $('#update-user-form').attr('action');
-
-    // Perform AJAX request
-    $.ajax({
-        url: formAction,
-        type: 'GET',
-        data: {
-            user_id: id,
-            action: 'delete_user'
-        }, // Form data
-        dataType: 'json',
-        success: function (response) {
-            if (response.success) {
-                setTimeout(function () {
-                    location.reload();
-                }, 1000);
-            }
-        },
-        error: function (error) {
-            // Handle the error
-            console.error('Error submitting the form:', error);
-        },
-        complete: function (response) {
-            // This will be executed regardless of success or error
-            console.log('Request complete:', response);
-        }
-    });
-}
--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,16 +1,19 @@
 <?php
-
+// Define an abstract class named BaseModel
 abstract class BaseModel
 {
-
+    // Protected property to hold the database manager instance
     protected $pm;
 
+    // Public properties representing common fields in database tables
     public $id;
     public $created_at;
     public $updated_at;
 
+    // Constructor method to initialize the database manager instance
     public function __construct()
     {
+        // Access the AppManager to get the database manager instance
         $this->pm = AppManager::getPM();
     }
 
@@ -23,6 +26,32 @@ abstract class BaseModel
     // Abstract method to update an existing record in the database (to be implemented by child classes)
     abstract protected function updateRec();
 
+    // Method to retrieve all records from the associated table
+    public function getAll()
+    {
+        return $this->pm->run("SELECT * FROM " . $this->getTableName());
+    }
+
+    // Method to retrieve all active records from the associated table
+    public function getAllActive()
+    {
+        return $this->pm->run("SELECT * FROM " . $this->getTableName() . " WHERE is_active = 1");
+    }
+
+    // Method to retrieve a record by its ID from the associated table
+    public function getById($id)
+    {
+        $param = array(':id' => $id);
+        return $this->pm->run("SELECT * FROM " . $this->getTableName() . " WHERE id = :id", $param, true);
+    }
+
+    // Method to retrieve all records based on a specific column and value from the associated table
+    public function getAllByColumnValue($column, $value)
+    {
+        return $this->pm->run("SELECT * FROM " . $this->getTableName() . " WHERE $column = $value");
+    }
+
+    // Method to save a record (either update an existing record or add a new one)
     public function save()
     {
         if (isset($this->id) && $this->id > 0) {
@@ -32,8 +61,10 @@ abstract class BaseModel
         }
     }
 
-    public function getAll()
+    // Method to delete a record by its ID from the associated table
+    public function deleteRec($id)
     {
-        return $this->pm->run("SELECT * FROM " . $this->getTableName());
+        $param = array(':id' => $id);
+        return $this->pm->run("DELETE FROM " . $this->getTableName() . " WHERE id = :id", $param);
     }
 }
